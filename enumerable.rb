3df2f57f45774b45adacf4f483e62f5f815e16eb
marwen-brini <brini.marwen@gmail.com>
2021-03-25 10:24:54 +0100

    added each with index

--- enumerable.rb
+++ enumerable.rb
@@ -1,9 +1,20 @@
+
 def my_each(arr)
     result = []
     length = arr.length-1
     for i in 0..length
-      result.push(arr[i])
+      result.push(yield(arr[i]))
     end
     result
 end
-puts my_each([1,2,3,4,5,6])
+
+def my_each_with_index(arr)
+    result = []
+    length = arr.length-1
+    for i in 0..length
+      result.push(yield(i,arr[i]))
+    end
+    result
+end
+
+my_each_with_index(["marwen","ali","salah"]){|i,v| puts"#{i} #{v}"}
