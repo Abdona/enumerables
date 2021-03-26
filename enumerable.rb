5b5065f8883d3b4e6183c1e196072de595e25622
Abdulrahman <boodynaser@rocketmail.com>
2021-03-26 03:22:40 +0200

    add self and allow bloc

--- enumerable.rb
+++ enumerable.rb
@@ -1,24 +1,25 @@
-def my_each(arr)
+module Enumerable
+def my_each
   result = []
-  length = arr.length - 1
+  length = self.length - 1
   (0..length).each do |i|
-    result.push(yield(arr[i]))
+    result.push(yield(self[i]))
   end
   result
 end
 
-def my_each_with_index(arr)
+def my_each_with_index
   result = []
-  length = arr.length - 1
+  length = self.length - 1
   (0..length).each do |i|
-    result.push(yield(i, arr[i]))
+    result.push(yield(i, self[i]))
   end
   result
 end
 
-def my_select(arr)
+def my_select
   result = []
-  my_each(arr) do |x|
+  self.my_each do |x|
     result.push(x) if yield(x)
   end
   result
@@ -26,43 +27,56 @@ end
 
 def my_map(arr)
   result = []
-  my_each(arr) do |x|
+  self.my_each do |x|
     result.push(yield(x))
   end
   result
 end
 
 def my_any?(arr)
-  my_each(arr) do |x|
+  self.my_each do |x|
     return true if yield(x)
   end
   false
 end
 
 def my_all?(arr)
-  my_each(arr) do |x|
+  self.my_each do |x|
     return false unless yield(x)
   end
   true
 end
 
 def my_none?(arr)
-  my_each(arr) do |x|
+  self.my_each do |x|
     return false if yield(x)
   end
   true
 end
 
 def my_inject?(arr, total)
-  my_each(arr) do |x|
+  self.my_each do |x|
     total = yield(total, x)
   end
   total
 end
 
-def my_count(arr)
-  count=arr.length
-  count
+def my_count
+  unless block_given?
+    return self.length
+  end
+  count = 0
+  self.my_each do |m|  #[1,2,3,2]
+    if m == yield
+      count += 1 
+    end
+  end
+  return count
+end
 end
 
-puts my_count([1,3,4,5])
+x = [1,3,4,5,5]
+y=x.my_count{5}
+p y
+
+p x.my_select {|x| x!=3}
