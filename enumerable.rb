79ae2fbc03969c5e27e167188e3aa53a5799c6dc
Abdulrahman <boodynaser@rocketmail.com>
2021-03-26 04:04:19 +0200

    correct robcoup errors

--- enumerable.rb
+++ enumerable.rb
@@ -1,86 +1,79 @@
 module Enumerable
-def my_each
-  if block_given?
+  def my_each
+    if block_given?
+      result = []
+      length = self.length - 1
+      (0..length).each do |i|
+        result.push(yield(self[i]))
+      end
+      result
+    end
+    "#<Enumerator: #{self}:my_each>"
+  end
+
+  def my_each_with_index
+    if block_given?
+      result = []
+      length = self.length - 1
+      (0..length).each do |i|
+        result.push(yield(i, self[i]))
+      end
+      result
+    end
+    "#<Enumerator: #{self}:my_each>"
+  end
+
+  def my_select
     result = []
-    length = self.length - 1
-    (0..length).each do |i|
-      result.push(yield(self[i]))
+    my_each do |x|
+      result.push(x) if yield(x)
     end
     result
   end
-  return "#<Enumerator: #{self}:my_each>"
-end
 
-def my_each_with_index
-  if block_given?
+  def my_map
     result = []
-    length = self.length - 1
-    (0..length).each do |i|
-      result.push(yield(i, self[i]))
+    my_each do |x|
+      result.push(yield(x))
     end
     result
   end
-  return "#<Enumerator: #{self}:my_each>"
-end
 
-def my_select
-  result = []
-  self.my_each do |x|
-    result.push(x) if yield(x)
-  end
-  result
-end
-
-def my_map(arr)
-  result = []
-  self.my_each do |x|
-    result.push(yield(x))
-  end
-  result
-end
-
-def my_any?(arr)
-  self.my_each do |x|
-    return true if yield(x)
-  end
-  false
-end
-
-def my_all?(arr)
-  self.my_each do |x|
-    return false unless yield(x)
-  end
-  true
-end
-
-def my_none?(arr)
-  self.my_each do |x|
-    return false if yield(x)
-  end
-  true
-end
-
-def my_inject?(arr, total)
-  self.my_each do |x|
-    total = yield(total, x)
-  end
-  total
-end
-
-def my_count
-  unless block_given?
-    return self.length
-  end
-  count = 0
-  self.my_each do |m|  #[1,2,3,2]
-    if m == yield
-      count += 1 
+  def my_any?
+    my_each do |x|
+      return true if yield(x)
     end
+    false
   end
-  return count
-end
-end
 
+  def my_all?
+    my_each do |x|
+      return false unless yield(x)
+    end
+    true
+  end
 
-s= [1,23,3].my_each
-puts s
+  def my_none?
+    my_each do |x|
+      return false if yield(x)
+    end
+    true
+  end
+
+  def my_inject?(_arr, total)
+    my_each do |x|
+      total = yield(total, x)
+    end
+    total
+  end
+
+  def my_count
+    return length unless block_given?
+
+    count = 0
+    my_each do |m|
+      count += 1 if yield == m
+    end
+    count
+  end
+end
